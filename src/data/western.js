ead0a83a7350bc53b63961842f14ba9cbb52afe8
gaultier-souris <gaultier.souris@gmail.com>
2024-05-21 11:07:46 +0200

    added assets

---
+++ src/data/western.js
@@ -0,0 +1,49 @@
+const western = [
+  {
+    id: 1,
+    image: '../../public/western/img/cheval.jpg',
+    sound: '../../public/western/sound/cheval.mp3',
+  },
+  {
+    id: 2,
+    image: '../../public/western/img/coup_de_feu.jpg',
+    sound: '../../public/western/sound/coup_de_feu.mp3',
+  },
+  {
+    id: 3,
+    image: '../../public/western/img/guitare.jpg',
+    sound: '../../public/western/sound/guitare.mp3',
+  },
+  {
+    id: 4,
+    image: '../../public/western/img/harmonica.jpg',
+    sound: '../../public/western/sound/harmonica.mp3',
+  },
+  {
+    id: 5,
+    image: '../../public/western/img/indien.jpg',
+    sound: '../../public/western/sound/indien.mp3',
+  },
+  {
+    id: 6,
+    image: '../../public/western/img/loup.jpg',
+    sound: '../../public/western/sound/loup.mp3',
+  },
+  {
+    id: 7,
+    image: '../../public/western/img/piano.jpg',
+    sound: '../../public/western/sound/piano.mp3',
+  },
+  {
+    id: 8,
+    image: '../../public/western/img/sifflet_de_train.jpg',
+    sound: '../../public/western/sound/sifflet_de_train.mp3',
+  },
+  {
+    id: 9,
+    image: '../../public/western/img/vache.jpg',
+    sound: '../../public/western/sound/vache.mp3',
+  },
+];
+
+export default western;
