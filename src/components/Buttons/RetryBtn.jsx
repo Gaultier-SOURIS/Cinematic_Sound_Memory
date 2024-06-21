bad9eeabdf58b62d6d9c03c33d1ad31df9b2991d
gaultier-souris <gaultier.souris@gmail.com>
2024-06-21 17:34:59 +0200

    error reload game

--- src/components/Buttons/RetryBtn.jsx
+++ src/components/Buttons/RetryBtn.jsx
@@ -2,8 +2,9 @@ import './RetryBtn.scss';
 
 export default function RetryBtn() {
   const handleReset = () => {
-    window.location.reload();
+    window.location.href = '/game';
   };
+
   return (
     <div className="retryBtn">
       <button className="startBtn__btn" type="button" onClick={handleReset}>
